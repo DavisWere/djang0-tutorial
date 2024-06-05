32b356bbcedfe941dece18832802494a7220b484
DavisWere <devisodhis10@gmail.com>
2024-06-05 12:20:57 +0300

    setup environment and model salary

---
+++ core/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Salary(models.Model):
+    employee_name = models.CharField(max_length=100)
+    base_salary = models.FloatField()
+    bonuses = models.FloatField(
+    )
+    deductions = models.FloatField(
+    )
+    net_salary = models.FloatField(
+        editable=False)
+
+    def save(self, *args, **kwargs):
+        self.net_salary = self.base_salary + self.bonuses - self.deductions
+        super(Salary, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.employee_name} - {self.net_salary}'
+
+# after writing , or modifying models we make migrations and then migrate
+# python manage.py makemigrations
+# python manage.py migrate
